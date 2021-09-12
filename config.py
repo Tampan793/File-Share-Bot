1ee25e0f513bd0bde1120740eeab50dd56753613
Tampan793 <69372834+Tampan793@users.noreply.github.com>
2021-09-12 18:12:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "10"))
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
