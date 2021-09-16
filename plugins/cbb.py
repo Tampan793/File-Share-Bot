7d04319eefbeba4d26161cdbc1106973a47b8943
Tampan793 <69372834+Tampan793@users.noreply.github.com>
2021-09-16 11:08:35 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @TiktokViral18_Pemersatu\n○ Support Helps : @xH4X0Rx</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Channel : @TiktokVirallHot2021\n○ Support Helps : @xH4X0Rx</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
